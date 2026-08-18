4d76c99952737e13b45d6f5b01997bd12faceb08
Mauricio Gandolfo Constante Neto <89039412+mgconstante@users.noreply.github.com>
2026-08-18 23:34:59 +0000

    Criação do Projeto

---
+++ ecomerce-back/src/main/java/br/edu/unifio/ecomerce_back/EcomerceBackApplication.java
@@ -0,0 +1,13 @@
+package br.edu.unifio.ecomerce_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomerceBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomerceBackApplication.class, args);
+	}
+
+}
---
+++ ecomerce-back/src/test/java/br/edu/unifio/ecomerce_back/EcomerceBackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.unifio.ecomerce_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomerceBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
